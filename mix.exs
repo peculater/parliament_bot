154cac0580199dd725b3f2e66db50b90ee408a4c
William Lieurance <william@namikoda.com>
2017-08-02 00:26:13 -0500

    Alphabetize the dependencies

--- mix.exs
+++ mix.exs
@@ -32,12 +32,12 @@ defmodule ParliamentBot.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
       {:phoenix, "~> 1.3.0"},
-      {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
+      {:phoenix_pubsub, "~> 1.0"},
       {:slack, "~> 0.12.0"}
     ]
   end
